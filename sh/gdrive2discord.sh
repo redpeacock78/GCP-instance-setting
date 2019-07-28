0028f46433d1c8d64c62580dba1cc7653057eb37
redpeacock78 <scarletpeacock78@gmail.com>
2019-07-28 13:01:37 +0900

    Now changing content: sh/gdrive2discord.sh

--- sh/gdrive2discord.sh
+++ sh/gdrive2discord.sh
@@ -1,15 +1,24 @@
 #!/usr/bin/env bash
 
 
+###FUNCTIONS###
+function dir_watch(){
+    inotifywait -r \
+                -e CREATE \
+                -e MODIFY \
+                -e DELETE \
+                --exclude "${IGNORE_DIR}" \
+                "${TARGET_DIR}"
+}
+
 
 ###PARAMETERS###
 TARGET_DIR="/home/redpeacock78/gdrive/handouts"
 IGNORE_DIR="/home/redpeacock78/gdrive/handouts/.git"
-GIT_LOG_DIR="/home/redpeacock78/gdrive/handouts/.git/objects"
-AUTHOR_NAME="$(cd ${TARGET_DIR} && git log -1 --pretty=format:'%aN')"
-GIT_MESSAGE="$(cd ${TARGET_DIR} && git log -1 --pretty=format:'%s')"
-GIT_HASH="$(cd ${TARGET_DIR} && git log -1 --pretty=format:'%h')"
-TIME_STAMP="$(cd ${TARGET_DIR} && date -d "$(git log -1 --pretty=format:'%cD')" +%s)"
+AUTHOR_NAME="$(dir_watch && git log -1 --pretty=format:'%aN' -C ${TARGET_DIR})"
+GIT_MESSAGE="$(dir_watch && git log -1 --pretty=format:'%s' -C ${TARGET_DIR})"
+GIT_HASH="$(dir_watch && git log -1 --pretty=format:'%h' -C ${TARGEY_DIR})"
+TIME_STAMP="$(dir_watch && date -d "$(git log -1 --pretty=format:'%cD' -C ${TARGET_DIR})" +%s)"
 AUTHOR_LINK="https://github.com/redpeacock78"
 ICON_LINK="https://avatars0.githubusercontent.com/u/31413765?s=460&v=4"
 GDRIVE_URL="https://drive.google.com/open?id=1wf0wE1dMbZU2fQfHGz1L1YK8HjAZpXYc"
@@ -18,17 +27,7 @@ WEBHOOK_URL="$(cat /home/redpeacock78/gcp-instance-setting/assets/webhooks.txt |
 
 ###TEXT###
 while true; do
-    inotifywait -r \
-		-e CREATE \
-		"${GIT_LOG_DIR}" \
-    && inotifywait -r \
-                   -e CREATE \
-		   -e MODIFY \
-		   -e DELETE \
-		   --exclude "${IGNORE_DIR}" \
-		   "${TARGET_DIR}" \
-    && sleep 30 \
-    && /usr/bin/echo -e '{ "attachments": [ { "fallback": "Required plain-text summary of the attachment.", "color": "#36a64f", "title": "[handouts] Update", "title_link": "'${GDRIVE_URL}'", "author_name": "'${AUTHOR_NAME}'", "author_link": "'${AUTHOR_LINK}'", "author_icon": "'${ICON_LINK}'", "text": "`'${GIT_HASH}'`: '${GIT_MESSAGE}'\\n- '${AUTHOR_NAME}'", "mrkdwn_in": [ "text" ], "ts": "'${TIME_STAMP}'" } ] }' \
+    /usr/bin/echo -e '{ "attachments": [ { "fallback": "Required plain-text summary of the attachment.", "color": "#36a64f", "title": "[handouts] Update", "title_link": "'${GDRIVE_URL}'", "author_name": "'${AUTHOR_NAME}'", "author_link": "'${AUTHOR_LINK}'", "author_icon": "'${ICON_LINK}'", "text": "`'${GIT_HASH}'`: '${GIT_MESSAGE}'\\n- '${AUTHOR_NAME}'", "mrkdwn_in": [ "text" ], "ts": "'${TIME_STAMP}'" } ] }' \
     | curl -H "Accept: application/json" \
            -H "Content-type: application/json" \
 	   -X POST \
