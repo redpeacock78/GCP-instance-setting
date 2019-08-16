93742a9bafab2b8902cbe5d12d7487718ef6eca5
redpeacock78 <scarletpeacock78@gmail.com>
2019-08-16 12:44:16 +0900

    Add content: GAS/sheet2calendar.js

---
+++ GAS/sheet2calendar.js
@@ -0,0 +1,20 @@
+var CALENDAR_ID = 'YOUR_CALENDAR_ID'
+var SHEET_NAME = 'YOUR_SHEET_NAME'
+
+function sheet2calendar() {
+  var sheet, i, myTitle, myDate, myDescription, added;
+  sheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName(SHEET_NAME);
+  for (var i = 2; i <= sheet.getLastRow(); i++) {
+    myTitle = sheet.getRange(i, 1).getValue();
+    myDate = sheet.getRange(i, 2).getValue();
+    myDescription = sheet.getRange(i, 3).getValue();
+    added = sheet.getRange(i, 4).getValue();
+    if(added == "") {
+      thisevent = CalendarApp.getCalendarById(CALENDAR_ID).createAllDayEvent(myTitle, myDate, {
+        description: myDescription
+      });
+      sheet.getRange(i, 4).setValue("完了");
+     }
+   }
+}
+
