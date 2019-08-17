1b2257e0351b5ba6ecbf75f1b888b3a874067512
redpeacock78 <scarletpeacock78@gmail.com>
2019-08-18 01:13:50 +0900

    Change content: script/module_output.py script/requirements.txt

--- script/module_output.py
+++ script/module_output.py
@@ -3,7 +3,14 @@
 '''
 HOWTO USE THIS SCRIPT
 
-$ ./module_output.py FILE_NAME | awk '/Loaded/,/-/' | sed 's/-*//g' | sed 's/: .*//g;s/\..*//g;s/^_.*//g;s/Loaded modules//' | sed '/^$/d' | sort | uniq | xargs -L1 -I{} bash -c 'pip3 freeze | grep {}' > requirements.txt
+$ ./module_output.py FILE_NAME \
+  > | awk '/Loaded/,/-/' \
+  > | sed 's/-*//g' \
+  > | sed 's/: .*//g;s/\..*//g;s/^_.*//g;s/Loaded modules//' \
+  > | sed '/^$/d' \
+  > | sort \
+  > | uniq \
+  > | xargs -L1 -I{} bash -c 'pip3 freeze | grep {}' > requirements.txt
 
 '''
 
