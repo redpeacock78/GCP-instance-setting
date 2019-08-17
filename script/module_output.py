b55563228223af4bc4ba9dbc0b23e5a2d8e0841c
redpeacock78 <scarletpeacock78@gmail.com>
2019-08-18 00:54:45 +0900

    Add content: script/module_output.py script/requirements.txt

---
+++ script/module_output.py
@@ -0,0 +1,25 @@
+#! /usr/bin/env python3 
+
+'''
+HOWTO USE THIS SCRIPT
+
+$ ./module_output.py FILE_NAME | awk '/Loaded/,/-/' | sed 's/-*//g' | sed 's/: .*//g;s/\..*//g;s/^_.*//g;s/Loaded modules//' | sed '/^$/d' | sort | uniq | xargs -L1 -I{} bash -c 'pip3 freeze | grep {}' > requirements.txt
+
+'''
+
+from modulefinder import ModuleFinder
+import sys
+import os
+
+file = sys.argv[1]
+finder = ModuleFinder()
+finder.run_script(file)
+
+print('Loaded modules')
+for name, mod in finder.modules.items():
+    print('%s: '% name, end='')
+    print(','.join(list(mod.globalnames.keys())[:3]))
+
+print('-'*50)
+print('Modules not imported:')
+print('\n'.join(finder.badmodules.keys()))
