e62239aeb872c53476e399eb8293e2176fdf93b9
redpeacock78 <scarletpeacock78@gmail.com>
2019-09-25 02:10:17 +0900

    Change content: sh/gdrive2discord.sh

--- sh/gdrive2discord.sh
+++ sh/gdrive2discord.sh
@@ -15,21 +15,16 @@ WEBHOOK_URL="$(cat /home/redpeacock78/gcp-instance-setting/assets/webhooks.txt |
 
 ###TEXT###
 while true; do
-    function dir_watch(){
-        inotifywait -r \
-	            -e CREATE \
-                    -e MODIFY \
-	            -e DELETE \
-	            --exclude "${IGNORE_DIR}" \
-	            "${TARGET_DIR}"
-    }
-    dir_watch && \
-    {
+    inotifywait -r \
+	        -e CREATE \
+                -e MODIFY \
+	        -e DELETE \
+	        --exclude "${IGNORE_DIR}" \
+	        "${TARGET_DIR}"
     AUTHOR_NAME="$(git log -1 --pretty=format:'%aN' -C ${TARGET_DIR})"
     GIT_MESSAGE="$(git log -1 --pretty=format:'%s' -C ${TARGET_DIR})"
     GIT_HASH="$(git log -1 --pretty=format:'%h' -C ${TARGEY_DIR})"
     TIME_STAMP="$(date -d "$(git log -1 --pretty=format:'%cD' -C ${TARGET_DIR})" +%s)"
-    } && \
     /usr/bin/echo -e '{ "attachments": [ { "fallback": "Required plain-text summary of the attachment.", "color": "#36a64f", "title": "[handouts] Update", "title_link": "'${GDRIVE_URL}'", "author_name": "'${AUTHOR_NAME}'", "author_link": "'${AUTHOR_LINK}'", "author_icon": "'${ICON_LINK}'", "text": "`'${GIT_HASH}'`: '${GIT_MESSAGE}'\\n- '${AUTHOR_NAME}'", "mrkdwn_in": [ "text" ], "ts": "'${TIME_STAMP}'" } ] }' \
     | curl -H "Accept: application/json" \
            -H "Content-type: application/json" \
